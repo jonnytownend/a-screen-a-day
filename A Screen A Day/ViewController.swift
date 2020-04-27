8d743a7267ce44160d569137dea2904c9734d8a5
Jonny Townend <jonathan.townend@gmail.com>
2020-04-27 15:14:52 +0100

    Initial Commit

---
+++ A Screen A Day/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  A Screen A Day
+//
+//  Created by Jonny Townend on 27/04/2020.
+//  Copyright © 2020 Brightec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
